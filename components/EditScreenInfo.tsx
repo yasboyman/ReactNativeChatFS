d039579a22831b01c36b2c161774bf30b5d73fd7
Yasir Alabadi <yasir@MMT-MAC-4396.local>
2022-11-14 18:36:53 +0000

    undone typo

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -27,7 +27,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+         Start chat by clicking on the button below
         </Text>
       </View>
 
@@ -44,7 +44,7 @@ export default function EditScreenInfo({ path }: { path: string }) {
 
 function handleHelpPress() {
   WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet'
+    'https://bbcnews.co.uk'
   );
 }
 
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -9,6 +9,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Text>Speak to us here</Text>
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
